7a3888dc8e88b616cbd4bf65ec0641b10bcc7e70
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-05-12 19:51:12 +0400

    update tag.py

--- tag.py
+++ tag.py
@@ -206,6 +206,64 @@ async def tag(event):
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
+        
+#########################
+
+#tek-tek tağ 
+
+@client.on(events.NewMessage(pattern="^/tektag ?(.*)"))
+async def tektag(event):
+  global Husu_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{noadmin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın!__")
+  else:
+    return await event.respond("__Tağ etməy üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Tək-Tək Tağ Başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                      Button.inline(f"{dayandir}", data="cancel")
+                      ]
+                    )
+                  ) 
+    Husu_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if event.chat_id not in Husu_tag:
+        await event.respond("⛔ Tək Tək Tağ Prosesi Dayandırıldı",
+                    buttons=(
+                      [
+                      Button.inline(f"{yeniden}", data="yeniden")
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
     
 #########################
 
