603d4eee710534426228a5fd3096e379cc869275
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-05-13 07:53:33 +0400

    update tag.py

--- tag.py
+++ tag.py
@@ -330,9 +330,60 @@ async def rtag(event):
 ürek = "❤️ 🧡 💛 💚 💙 💜 🖤 💘 💝 ❤️        🧡 💛 💚 💙 💜 🖤 💘 💝",
         .split(" ") 
         
+@client.on(events.NewMessage(pattern="^/utag ?(.*)"))
+async def utag(event):
+  global Husu_tag
+  if event.is_private:
+    return await event.respond(f"{noqrup}")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond(f"{noadmin}")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("__Köhnə mesajları görə bilmirəm! (bu mesaj məni qrupa əlavə etməmişdən qabaq yazılıb)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Tağ mesajı yazmadın!__")
+  else:
+    return await event.respond("__Tağ etməy üçün bir mesaj yanıtlayın və ya bir mətn yazın!__")
+    
+  if mode == "text_on_cmd":
+    await client.send_message(event.chat_id, "❄️ Rənglərlə Tağ Başladı\n⏱️ İnterval - 2 saniyə",
+                    buttons=(
+                      [
+                      Button.inline(f"dayandir", data="cancel")
+                      ]
+                    )
+                  ) 
+    Husu_tag.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(ürek)}](tg://user?id={usr.id}) "
+      if event.chat_id not in Husu_tag:
+        await event.respond("⛔ Rənglərlə Tağ Prosesi Dayandırıldı",
+                    buttons=(
+                      [
+                      Button.inline(f"🙄Təmirdə", data="yeniden")
+                      ]
+                    )
+                  )
+        return
+      if usrnum == 3:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""        
         
-        
-reqem = "🔟 9️⃣ 8️⃣ 7️⃣ 6️⃣ 5️⃣ 4️⃣ 3️⃣        2️⃣ 1️⃣ 0️⃣ "
 
 
 
