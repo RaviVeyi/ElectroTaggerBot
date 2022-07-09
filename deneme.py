7b41824c8549f5134b655a6035844b93483b5d22
emirpng <marveldarkbull@gmail.com>
2022-07-09 23:45:58 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -3,11 +3,14 @@
 @client.on(events.NewMessage(pattern="^/start$"))
 async def prmsg(event: events.NewMessage.Event):
   if event.is_group:
-    return await client.send_message(event.chat_id, f"{qrupstart}",
+    return await client.send_message(event.chat_id, f"Bir rol takımı seçin.",
 buttons=(
                       [
-                       Button.inline('📮 Komutlar ', data='grkomut'),
-                       Button.url('👨🏻‍💻 Sahibim', f'https://t.me/{sahib}')
+                       Button.inline('👱 Köylü Takımı', data='koylu')
+                      ],
+                      [
+                       Button.inline('🐺 Kurt Takımı', data='kurt'),
+                       Button.inline('👤 Bireysel', data='bireysel')
                       ],
                     ),
                     link_preview=False)
@@ -18,22 +21,75 @@ buttons=(
 @client.on(events.callbackquery.CallbackQuery(data="grstart"))
 async def start(event):
     async for usr in client.iter_participants(event.chat_id):
-     await event.edit(f"{qrupstart}",
+     await event.edit(f"Bir rol takımı seçin.",
                     buttons=(
-			[
-                       Button.inline('📮 Komutlar ', data='grkomut'),
-                       Button.url('👨🏻‍💻 Sahibim', f'https://t.me/{sahib}')
+                      [
+                       Button.inline('👱 Köylü Takımı', data='koylu')
+                      ],
+                      [
+                       Button.inline('🐺 Kurt Takımı', data='kurt'),
+                       Button.inline('👤 Bireysel', data='bireysel')
                       ],
                     ),
-                    link_preview=False
-                   )
+                    link_preview=False)
 
 
 
 # Grup mesaj geri dön butonu
-@client.on(events.callbackquery.CallbackQuery(data="grkomut"))
+@client.on(events.callbackquery.CallbackQuery(data="koylu"))
 async def handler(event):
-    await event.edit(f"{komutlar}", buttons=(
+    await event.edit(f"Hakkında bilgi almak istediğiniz rolü seçin.", buttons=(
+                      [
+                      Button.inline("Tarikat Avcısı 💂", data="tavci")
+                      ],
+                      [
+                      Button.inline("Gözcü 👳", data="gozcu")
+                      Button.inline("Sarhoş 🍻", data="sarhos")
+                      ],
+                      [
+                      Button.inline("Yancı 💋", data="yancı")
+                      Button.inline("Seyirci 👁", data="seyirci")
+                      ],
+                      [
+                      Button.inline("Silahşör 🔫", data="silahsor")
+                      Button.inline("Koruyucu Melek 👼", data="kmelek")
+                      ],
+                      [
+                      Button.inline("Mason 👷", data="mason")
+                      Button.inline("Dedektif 🕵️", data="dedektif")
+                      ],
+                      [
+                      Button.inline("Lanetli 😾", data="lanetli")
+                      Button.inline("Avcı 🎯", data="avci")
+                      ],
+                      [
+                      Button.inline("Eros 🏹", data="eros")
+                      Button.inline("Demirci ⚒️", data="demirci")
+                      ],
+                      [
+                      Button.inline("Prens 💍", data="prens")
+                      Button.inline("Muhtar 🎖", data="muhtar")
+                      ],
+                      [
+                      Button.inline("Kahin 🌀", data="kahin")
+                      Button.inline("Hükümdar 👑", data="hükümdar")
+                      ],
+                      [
+                      Button.inline("Barışçıl ☮️", data="barışçıl")
+                      Button.inline("Yaşlı Bilge 📚", data="ybilge")
+                      ],
+                      [
+                      Button.inline("Uyutucu 💤", data="uyutucu")
+                      Button.inline("Fedai 🔰", data="fedai")
+                      ],
+                      [
+                      Button.inline("Simyacı 🍵", data="simyaci")
+                      Button.inline("Güzel 💅", data="guzel")
+                      ],
+                      [
+                      Button.inline("Fırtına Getiren 🌩", data="fırtına")
+                      Button.inline("Yabanı Çocuk 👶", data="yabani")
+                      ],
                       [
                       Button.inline("👈 Geri", data="grstart")
                       ]
